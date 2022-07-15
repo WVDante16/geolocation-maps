12174247c3dda98e817067ddbd27d6869941a6a4
unknown <walbertozekrom@gmail.com>
2022-07-15 11:05:43 -0500

    a

--- App.js
+++ App.js
@@ -1,63 +1,81 @@
-import React, {useState, useEffect} from 'react';
-import {Text, View} from 'react-native';
-import * as Location from 'expo-location';
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
 import MapView from 'react-native-maps';
 import styles from './styles';
 
-const API_KEY = "";
-const URL = `https://maps.google.com/maps/api/geocode/json?key=${API_KEY}&latlng=`;
+const hotelRegion = {
+  coordinates: [
+    {latitude: 20.672747278747842, longitude: -103.37539720734652},
+    {latitude: 20.669673831926215, longitude: -103.37494960785168},
+    {latitude: 20.67112893574988, longitude: -103.36900943150502},
+    {latitude: 20.673213307650773, longitude: -103.37109064046835},
+    {latitude: 20.672747278747842, longitude: -103.37539720734652},
+  ],
+
+  strokeColor: "#008080",
+  strokeWidth: 4,
+}
+
+const barRegion = {
+  coordinates: [
+    {latitude: 20.671145466079626, longitude: -103.37277506730538},
+    {latitude: 20.667069928972786, longitude: -103.37072585978443},
+    {latitude: 20.66866100757824, longitude: -103.36802219329526},
+    {latitude: 20.671120370739587, longitude: -103.36820458349344},
+    {latitude: 20.671145466079626, longitude: -103.37277506730538},
+  ],
+
+  strokeColor: "firebrick",
+  strokeWidth: 4,
+}
 
 export default function App() {
-  const [address, setAddress] = useState("loading...");
-  const [longitude, setLongitude] = useState();
-  const [latitude, setLatitude] = useState();
+  const [hotelStyles, setHotelStyles] = useState([styles.hotelText, styles.boldText]);
+  const [barStyles, setBarStyles] = useState([styles.barText]);
+  const [overlays, setOverlays] = useState([hotelRegion]);
 
-  useEffect(() => {
-    function setPosition({coords: {latitude, longitude}}) {
-      setLongitude(longitude);
-      setLatitude(latitude);
-      fetch(`${URL}${latitude}${longitude}`)
-        .then((resp) => resp.json())
-        .then(({results}) => {
-          if(results.length > 0) {
-            setAddress(results[0].formatted_address);
-          }
-        })
-        .catch((error) => {
-          console.log(error.message);
-        });
-    }
+  function onClickhotel() {
+    setHotelStyles([...hotelStyles, styles.boldText]);
+    setBarStyles([barStyles[0]]);
+    setOverlays([hotelRegion]);
+  }
 
-    let watcher;
-
-    (async () => {
-      let {status} = await Location.requestForegroundPermissionsAsync();
-
-      if (status !== "granded") {
-        setErrorMsg("Permission denied");
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setPosition(location);
-
-      watcher = await Location.watchPositionAsync(
-        {accuracy: Location.LocationAccuracy.Highest}, 
-        setPosition
-      );
-    })();
-
-    return () => {
-      watcher?.remove();
-    };
-  }, []);
-
+  function onClickbar() {
+    setBarStyles([...barStyles, styles.boldText]);
+    setHotelStyles([hotelStyles[0]]);
+    setOverlays([barRegion]);
+  }
+  
   return (
     <View style={styles.container}>
-      <Text style={styles.address}>Adress: {address}</Text>
-      <Text style={styles.label}>Latitude: {latitude}</Text>
-      <Text style={styles.label}>Longitude: {longitude}</Text>
-      <MapView style={styles.mapView} showsUserLocation FollowUserLocation />
+      <View>
+        <Text style={hotelStyles} onPress={onClickhotel}>
+          Hoteles Y Restaurantes
+        </Text>
+        <Text style={barStyles} onPress={onClickbar}>
+          Antros Y Bares
+        </Text>
+      </View>
+
+      <MapView
+        style = {styles.mapView}
+        initialRegion = {{
+          latitude: 20.672747278747842,
+          longitude: -103.37539720734652,
+
+          latitudeDelta: 0.002,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {overlays.map((v, i) => (
+          <MapView.Polygon 
+            key = {i}
+            coordinates = {v.coordinates}
+            strokeColor = {v.strokeColor}
+            strokeWidth = {v.strokeWidth}
+          />
+        ))}
+      </MapView>
     </View>
-  )
+  );
 }
--- styles.js
+++ styles.js
@@ -8,18 +8,21 @@ export default StyleSheet.create({
         backgroundColor: 'ghostwhite',
     },
 
-    label: {
-        textAlign: 'center',
-        margin: 10,
-    },
-
-    address: {
-        fontWeight: 'bold',
-    },
-
     mapView: {
         alignSelf: 'stretch',
         height: 450,
         margin: 30
+    },
+
+    hotelText: {
+        color: "#008080"
+    },
+
+    barText: {
+        color: "#800000"
+    },
+
+    boldText: {
+        fontWeight: 'bold',
     }
 })
